84d1696c54b72cefa2e9ccb9df39b465fcd65c19
LuisChavezz <ing.luischavezz@gmail.com>
2021-08-08 17:06:32 -0500

    peticiones y promesas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 
-import './styles.css';
+const jorkeUrl = 'https://api.chucknorris.io/jokes/random';
 
+/*fetch( jorkeUrl ).then( response => {
+
+    response.json().then( data => { // .json() extrae el 'body' de la petición
+        console.log(data); // data guardará el objeto json (body de la petición)
+        console.log(data.value);
+    });
+});*/
+
+// (forma más limpia de trabajarlo)
+fetch( jorkeUrl ) 
+    .then( response => response.json() ) // promesa que regresa otra promesa (promesa dentro de otra)
+    .then( data => { // el 'then' de la segunda promesa
+        console.log(data);
+        console.log(data.value);
+    }); 
