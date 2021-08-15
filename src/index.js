dd4280cc8f8defb1027929ac8fdba4103e2683ed
LuisChavezz <ing.luischavezz@gmail.com>
2021-08-14 19:28:37 -0500

    Vista del input para subir imágen

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@
 
 //import { init } from "./js/jokes-page";
 
+import { init3 } from "./js/files-page";
+
 //import { init2 } from "./js/usuarios-page";
 
-import * as CRUD from './js/crud-provider';
+//import * as CRUD from './js/crud-provider';
 
 //init();
 //init2();
 
-CRUD.getUser( 1 ).then( console.log );
+/*CRUD.getUser( 1 ).then( console.log );
 
 CRUD.createUser({
     name: 'Fernando',
@@ -21,4 +23,6 @@ CRUD.updateUser( 1, {
     job: 'Facturista',
 }).then( console.log );
 
-CRUD.deleteUser( 1 ).then( console.log );
+CRUD.deleteUser( 1 ).then( console.log );*/
+
+init3();
---
+++ src/js/files-page.js
@@ -0,0 +1,45 @@
+
+const body = document.body;
+let inputFile, imgPhoto;
+
+const createInputFileHtml = () => {
+
+    const html = `
+        <h1 class="mt-5">Upload files</h1>
+        <hr>
+
+        <label>upload image: </label>
+
+        <input type="file" accept="image/png, image/jpeg" />
+
+        <br>
+        <img id="foto" class="img-thumbail" src="">
+    `;
+
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    body.append(div);
+
+    inputFile = document.querySelector( 'input' );
+    imgPhoto  = document.querySelector( '#foto' );
+    
+}
+
+const events = () => {
+
+    inputFile.addEventListener( 'change', ( event ) => {
+
+        const file = event.target.files[0]; // guarda el 'file' en la posición 0 del arreglo 'files' que contiene el evento
+        console.log( file );
+
+    });
+
+}
+
+
+
+
+export const init3 = () => {
+    createInputFileHtml();
+    events();
+}
